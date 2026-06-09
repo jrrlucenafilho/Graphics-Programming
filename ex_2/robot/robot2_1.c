e0b950f800298a186cde07be1af9237cd1a55756
jrrlucenafilho <jrrlucenafilho@gmail.com>
2026-06-09 10:58:36 -0300

    refactor(robot): aplicar formatação consistente ao código

--- ex_2/robot/robot2_1.c
+++ ex_2/robot/robot2_1.c
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 1993-1997, Silicon Graphics, Inc.
- * ALL RIGHTS RESERVED 
- * Permission to use, copy, modify, and distribute this software for 
+ * ALL RIGHTS RESERVED
+ * Permission to use, copy, modify, and distribute this software for
  * any purpose and without fee is hereby granted, provided that the above
  * copyright notice appear in all copies and that both the copyright notice
- * and this permission notice appear in supporting documentation, and that 
+ * and this permission notice appear in supporting documentation, and that
  * the name of Silicon Graphics, Inc. not be used in advertising
  * or publicity pertaining to distribution of the software without specific,
- * written prior permission. 
+ * written prior permission.
  *
  * THE MATERIAL EMBODIED ON THIS SOFTWARE IS PROVIDED TO YOU "AS-IS"
  * AND WITHOUT WARRANTY OF ANY KIND, EXPRESS, IMPLIED OR OTHERWISE,
@@ -21,8 +21,8 @@
  * ADVISED OF THE POSSIBILITY OF SUCH LOSS, HOWEVER CAUSED AND ON
  * ANY THEORY OF LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE
  * POSSESSION, USE OR PERFORMANCE OF THIS SOFTWARE.
- * 
- * US Government Users Restricted Rights 
+ *
+ * US Government Users Restricted Rights
  * Use, duplication, or disclosure by the Government is subject to
  * restrictions set forth in FAR 52.227.19(c)(2) or subparagraph
  * (c)(1)(ii) of the Rights in Technical Data and Computer Software
@@ -47,85 +47,80 @@
 
 static int shoulder = 0, elbow = 0;
 
-void init(void) 
-{
-   glClearColor (0.0, 0.0, 0.0, 0.0);
-   glShadeModel (GL_FLAT);
+void init(void) {
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glShadeModel(GL_FLAT);
 }
 
-void display(void)
-{
-   glClear (GL_COLOR_BUFFER_BIT);
-   glPushMatrix();
-   glTranslatef (-1.0, 0.0, 0.0);
-   glRotatef ((GLfloat) shoulder, 0.0, 0.0, 1.0);
-   glTranslatef (1.0, 0.0, 0.0);
-   glPushMatrix();
-   glScalef (2.0, 0.4, 1.0);
-   glutWireCube (1.0);
-   glPopMatrix();
+void display(void) {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glPushMatrix();
+  glTranslatef(-1.0, 0.0, 0.0);
+  glRotatef((GLfloat)shoulder, 0.0, 0.0, 1.0);
+  glTranslatef(1.0, 0.0, 0.0);
+  glPushMatrix();
+  glScalef(2.0, 0.4, 1.0);
+  glutWireCube(1.0);
+  glPopMatrix();
 
-   glTranslatef (1.0, 0.0, 0.0);
-   glRotatef ((GLfloat) elbow, 0.0, 0.0, 1.0);
-   glTranslatef (1.0, 0.0, 0.0);
-   glPushMatrix();
-   glScalef (2.0, 0.4, 1.0);
-   glutWireCube (1.0);
-   glPopMatrix();
+  glTranslatef(1.0, 0.0, 0.0);
+  glRotatef((GLfloat)elbow, 0.0, 0.0, 1.0);
+  glTranslatef(1.0, 0.0, 0.0);
+  glPushMatrix();
+  glScalef(2.0, 0.4, 1.0);
+  glutWireCube(1.0);
+  glPopMatrix();
 
-   glPopMatrix();
-   glutSwapBuffers();
+  glPopMatrix();
+  glutSwapBuffers();
 }
 
-void reshape (int w, int h)
-{
-   glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
-   glMatrixMode (GL_PROJECTION);
-   glLoadIdentity ();
-   gluPerspective(65.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
-   glTranslatef (0.0, 0.0, -5.0);
+void reshape(int w, int h) {
+  glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(65.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glTranslatef(0.0, 0.0, -5.0);
 }
 
-void keyboard (unsigned char key, int x, int y)
-{
-   switch (key) {
-      case 's':
-         shoulder = (shoulder + 5) % 360;
-         glutPostRedisplay();
-         break;
-      case 'S':
-         shoulder = (shoulder - 5) % 360;
-         glutPostRedisplay();
-         break;
-      case 'e':
-         elbow = (elbow + 5) % 360;
-         glutPostRedisplay();
-         break;
-      case 'E':
-         elbow = (elbow - 5) % 360;
-         glutPostRedisplay();
-         break;
-      case 27:
-         exit(0);
-         break;
-      default:
-         break;
-   }
+void keyboard(unsigned char key, int x, int y) {
+  switch (key) {
+  case 's':
+    shoulder = (shoulder + 5) % 360;
+    glutPostRedisplay();
+    break;
+  case 'S':
+    shoulder = (shoulder - 5) % 360;
+    glutPostRedisplay();
+    break;
+  case 'e':
+    elbow = (elbow + 5) % 360;
+    glutPostRedisplay();
+    break;
+  case 'E':
+    elbow = (elbow - 5) % 360;
+    glutPostRedisplay();
+    break;
+  case 27:
+    exit(0);
+    break;
+  default:
+    break;
+  }
 }
 
-int main(int argc, char** argv)
-{
-   glutInit(&argc, argv);
-   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowSize (500, 500); 
-   glutInitWindowPosition (100, 100);
-   glutCreateWindow (argv[0]);
-   init ();
-   glutDisplayFunc(display); 
-   glutReshapeFunc(reshape);
-   glutKeyboardFunc(keyboard);
-   glutMainLoop();
-   return 0;
+int main(int argc, char **argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow(argv[0]);
+  init();
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutKeyboardFunc(keyboard);
+  glutMainLoop();
+  return 0;
 }
